f8baf8e49b5844fd43dd07bd6856555569d245ee
Liu Awen <157514367@qq.com>
2020-02-01 22:25:18 +0800

    singleton pattern （单例模式）

---
+++ DesignPattern/src/main/java/ZenofDesignPatterns/singleton/section4/Singleton.java
@@ -0,0 +1,21 @@
+package ZenofDesignPatterns.singleton.section4;
+
+/**
+ *
+ */
+public final class Singleton {
+	private static Singleton singleton = null;
+	
+	//限制产生多个对象
+	private Singleton(){
+		
+	}
+	
+	//通过该方法获得实例对象
+	public synchronized static Singleton getSingleton(){
+		if(singleton == null){
+			singleton = new Singleton();
+		}
+		return singleton;		
+	}
+}
